5bffe639c279696f2bd2f89b2e64fdbb85677231
daylonjhons <daylonjhons@gmail.com>
2024-03-23 22:25:27 -0500

    added crud functions for departments, roles, and employees

--- index.js
+++ index.js
@@ -71,3 +71,192 @@ function runApp() {
         }
     });
 }
+
+function viewAllDepartments() {
+    connection.query("SELECT * FROM department", function (err, data) {
+        if (err) {
+            console.error("Error retrieving departments data:", err);
+            return;
+        }
+        console.table(data);
+        runApp();
+    });
+}
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "What is the department name?"
+        }
+    ]).then(function (res) {
+        connection.query("INSERT INTO department (name) VALUES (?)", [res.department], function (err, data) {
+            if (err) {
+                console.error("Error adding department:", err);
+                return;
+            }
+            console.log("Added Department");
+            runApp();
+        });
+    });
+}
+
+function viewAllRoles() {
+    connection.query("SELECT * FROM role", function (err, data) {
+        if (err) {
+            console.error("Error retrieving roles data:", err);
+            return;
+        }
+        console.table(data);
+        runApp();
+    });
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            message: "Enter role title:",
+            type: "input",
+            name: "title"
+        },
+        {
+            message: "Enter salary:",
+            type: "number",
+            name: "salary"
+        },
+        {
+            message: "Enter department ID:",
+            type: "number",
+            name: "department_id"
+        }
+    ]).then(function (res) {
+        connection.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [res.title, res.salary, res.department_id], function (err, data) {
+            if (err) {
+                console.error("Error adding role:", err);
+                return;
+            }
+            console.log("Added Role");
+            runApp();
+        });
+    });
+}
+
+function viewAllEmployees() {
+    const query = `
+        SELECT 
+            e.id AS id,
+            e.first_name AS first_name,
+            e.last_name AS last_name,
+            r.title AS title,
+            d.name AS department,
+            r.salary AS salary,
+            CONCAT(m.first_name, ' ', m.last_name) AS manager
+        FROM 
+            employee e
+        INNER JOIN 
+            role r ON e.role_id = r.id
+        INNER JOIN 
+            department d ON r.department_id = d.id
+        LEFT JOIN 
+            employee m ON e.manager_id = m.id`;
+
+    connection.query(query, function (err, data) {
+        if (err) {
+            console.error("Error retrieving employee data:", err);
+            return;
+        }
+        console.table(data);
+        runApp();
+    });
+}
+
+function addEmployee() {
+    connection.query("SELECT id, title FROM role", function (err, roles) {
+        if (err) {
+            console.error("Error retrieving role data:", err);
+            return;
+        }
+
+        connection.query("SELECT id, CONCAT(first_name, ' ', last_name) AS manager_name FROM employee WHERE manager_id IS NULL", function (err, managers) {
+            if (err) {
+                console.error("Error retrieving manager data:", err);
+                return;
+            }
+
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "firstName",
+                    message: "What is the employee's first name?"
+                },
+                {
+                    type: "input",
+                    name: "lastName",
+                    message: "What is the employee's last name?"
+                },
+                {
+                    type: "list",
+                    name: "roleId",
+                    message: "Select the employee's role:",
+                    choices: roles.map(role => ({ name: role.title, value: role.id }))
+                },
+                {
+                    type: "list",
+                    name: "managerId",
+                    message: "Select the employee's manager:",
+                    choices: managers.map(manager => ({ name: manager.manager_name, value: manager.id }))
+                }
+            ]).then(function (res) {
+                connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [res.firstName, res.lastName, res.roleId, res.managerId], function (err, data) {
+                    if (err) {
+                        console.error("Error adding employee:", err);
+                        return;
+                    }
+                    console.log("Added Employee");
+                    runApp();
+                });
+            });
+        });
+    });
+}
+
+function updateEmployeeRole() {
+    connection.query("SELECT id, CONCAT(first_name, ' ', last_name) AS full_name FROM employee WHERE manager_id IS NOT NULL", function (err, employees) {
+        if (err) {
+            console.error("Error retrieving employee data:", err);
+            return;
+        }
+
+        connection.query("SELECT id, title FROM role", function (err, roles) {
+            if (err) {
+                console.error("Error retrieving role data:", err);
+                return;
+            }
+
+            inquirer.prompt([
+                {
+                    message: "Which employee's role do you want to update?",
+                    type: "list",
+                    name: "employeeId",
+                    choices: employees.map(emp => ({ name: emp.full_name, value: emp.id }))
+                },
+                {
+                    message: "Select the new role:",
+                    type: "list",
+                    name: "role_id",
+                    choices: roles.map(role => ({ name: role.title, value: role.id }))
+                }
+            ]).then(function (res) {
+                connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [res.role_id, res.employeeId], function (err, data) {
+                    if (err) {
+                        console.error("Error updating employee role:", err);
+                        return;
+                    }
+                    console.log("Updated Employee Role");
+                    runApp();
+                });
+            });
+        });
+    });
+}
